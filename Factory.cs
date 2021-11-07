41b0d6612f9c4df0df52c1b0cad2560aac7068ae
Jujstme <e.zaffuto@gmail.com>
2021-11-07 18:32:44 +0100

    Fixed small mistake

--- Factory.cs
+++ Factory.cs
@@ -10,13 +10,13 @@ namespace LiveSplit.KulaWorld
 {
     public class KulaWorldFactory : IComponentFactory
     {
-        public string ComponentName => "Team Sonic Racing Autosplitter";
+        public string ComponentName => "Kula World Autosplitter";
         public string Description => "Automatic splitting and IGT calculation";
         public ComponentCategory Category => ComponentCategory.Control;
         public string UpdateName => this.ComponentName;
-        public string UpdateURL => "https://raw.githubusercontent.com/Jujstme/LiveSplit.TeamSonicRacing/master/";
+        public string UpdateURL => "https://raw.githubusercontent.com/Jujstme/LiveSplit.KulaWorld/master/";
         public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
-        public string XMLURL => this.UpdateURL + "Components/update.LiveSplit.TeamSonicRacing.xml";
+        public string XMLURL => this.UpdateURL + "Components/update.LiveSplit.KulaWorld.xml";
         public IComponent Create(LiveSplitState state) { return new Component(state); }
     }
 }
